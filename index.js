69e63d7dbb6c838ab5ece20fc5b9b5671134b747
Michael Pradetto <pradetto5@gmail.com>
2023-04-25 23:29:05 -0500

    updated cors to allow website

--- index.js
+++ index.js
@@ -26,7 +26,18 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://resumize.netlify.app",
+      ];
+
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
